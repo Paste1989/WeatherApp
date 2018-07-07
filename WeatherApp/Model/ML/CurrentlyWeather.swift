bf43b01d7e64392331c45bce1876cbff418c89c8
Sasa <brezovac.sasa@gmail.com>
2018-07-07 11:22:41 +0200

    first commit

---
+++ WeatherApp/Model/ML/CurrentlyWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentlyWeather.swift
+//  WeatherApp
+//
+//  Created by Brezonje on 06.07.2018..
+//  Copyright © 2018. CopyPaste89. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/Model/ML/DailyWeather.swift
@@ -0,0 +1,9 @@
+//
+//  DailyWeather.swift
+//  WeatherApp
+//
+//  Created by Brezonje on 06.07.2018..
+//  Copyright © 2018. CopyPaste89. All rights reserved.
+//
+
+import Foundation
