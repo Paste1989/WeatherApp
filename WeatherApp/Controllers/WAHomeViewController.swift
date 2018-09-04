18e84fea82979d85e7cf2075317d3021943a36ff
Sasa <brezovac.sasa@gmail.com>
2018-09-04 12:44:20 +0200

    WeatherTypeEnum

--- WeatherApp/Controllers/WAHomeViewController.swift
+++ WeatherApp/Controllers/WAHomeViewController.swift
@@ -12,6 +12,52 @@ import IQKeyboardManagerSwift
 
 class WAHomeViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
     
+    enum WeatherType {
+        case clearDay
+        case clearNight
+        case cloudy
+        case fog
+        case hail
+        case partlyCloudyDay
+        case partlyCloudyNight
+        case rain
+        case sleet
+        case snow
+        case thunderstorm
+        case tornado
+        case wind
+        
+        static func getWeatherType(type: WeatherType) -> String? {
+            switch type {
+            case .clearDay:
+                return "clear-day"
+            case .clearNight:
+                return "clear-night"
+            case .cloudy:
+                return "cloudy"
+            case .fog:
+                return "fog"
+            case .hail:
+                return "hail"
+            case .partlyCloudyDay:
+                return "partly-cloudy-day"
+            case .partlyCloudyNight:
+                return "partly-cloudy-night"
+            case .rain:
+                return "rain"
+            case .sleet:
+                return "sleet"
+            case .snow:
+                return "snow"
+            case .thunderstorm:
+                return "thunderstorm"
+            case .tornado:
+                return "tornado"
+            case .wind:
+                return "wind"
+            }
+        }
+    }
     
     var searchVCLocationsToShow = [Location]()
     var settingsVCLocationsToShow = [Location]()
@@ -493,47 +539,108 @@ class WAHomeViewController: UIViewController, UITextFieldDelegate, UITableViewDe
                     
                     
                     DispatchQueue.main.async {
-                        self.headerImageView.image = UIImage(named: "header_image-\(iconData)")
-                        self.bodyImageView.image = UIImage(named: "body_image-\(iconData)")
                         
-                        
-                        if iconData == "clear-day" {
+                        let clearDay: String = WeatherType.getWeatherType(type: .clearDay)!
+                        if clearDay == iconData {
+   
+                            self.headerImageView.image = UIImage(named: "header_image-\(clearDay)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(clearDay)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x59B7E0).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
-                        if iconData == "clear-night" {
+                        
+                        let clearNight: String = WeatherType.getWeatherType(type: .clearNight)!
+                        if clearNight == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(clearNight)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(clearNight)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x044663).cgColor, UIColor(hex: 0x234880).cgColor)
                         }
-                        if iconData == "cloudy" {
+                        
+                        let cloudy: String = WeatherType.getWeatherType(type: .cloudy)!
+                        if cloudy == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(cloudy)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(cloudy)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x59B7E0).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
-                        else if iconData == "fog" {
+                        
+                        let fog: String = WeatherType.getWeatherType(type: .fog)!
+                        if fog == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(fog)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(fog)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0xABD6E9).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
-                        else if iconData == "hail" {
+                        
+                        let hail: String = WeatherType.getWeatherType(type: .hail)!
+                        if hail == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(hail)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(hail)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x59B7E0).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
-                        else if iconData == "partly-cloudy-day" {
+                        
+                        let partlyCloudyDay: String = WeatherType.getWeatherType(type: .partlyCloudyDay)!
+                        if partlyCloudyDay == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(partlyCloudyDay)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(partlyCloudyDay)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x59B7E0).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
-                        else if iconData == "partly-cloudy-night" {
+                        
+                        let partlyCloudyNight: String = WeatherType.getWeatherType(type: .partlyCloudyNight)!
+                        if partlyCloudyNight == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(partlyCloudyNight)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(partlyCloudyNight)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x044663).cgColor, UIColor(hex: 0x234880).cgColor)
                         }
-                        else if iconData == "rain" {
+                        
+                        let rain: String = WeatherType.getWeatherType(type: .rain)!
+                        if rain == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(rain)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(rain)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x15587B).cgColor, UIColor(hex: 0x4A75A2).cgColor)
                         }
-                        else if iconData == "sleet" {
+                        
+                        let sleet: String = WeatherType.getWeatherType(type: .sleet)!
+                        if sleet == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(sleet)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(sleet)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x59B7E0).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
-                        else if iconData == "snow" {
+                        
+                        let snow: String = WeatherType.getWeatherType(type: .snow)!
+                        if snow == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(snow)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(snow)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x0B3A4E).cgColor, UIColor(hex: 0x80D5F3).cgColor)
                         }
-                        else if iconData == "thunderstorm" {
+                        
+                        let thunderstorm: String = WeatherType.getWeatherType(type: .thunderstorm)!
+                        if thunderstorm == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(thunderstorm)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(thunderstorm)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x15587B).cgColor, UIColor(hex: 0x4A75A2).cgColor)
                         }
-                        else if iconData == "tornado" {
+                        
+                        let tornado: String = WeatherType.getWeatherType(type: .tornado)!
+                        if tornado == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(tornado)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(tornado)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x15587B).cgColor, UIColor(hex: 0x4A75A2).cgColor)
                         }
-                        else if iconData == "wind" {
+                        
+                        let wind: String = WeatherType.getWeatherType(type: .wind)!
+                        if wind == iconData {
+                            
+                            self.headerImageView.image = UIImage(named: "header_image-\(wind)")
+                            self.bodyImageView.image = UIImage(named: "body_image-\(wind)")
                             self.skyColorImageView.layer.configureGradientBackground(UIColor(hex: 0x59B7E0).cgColor, UIColor(hex: 0xD8D8D8).cgColor)
                         }
                     }
